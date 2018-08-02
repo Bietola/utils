6d31317c3666077e52d7c24ba05eb2bc128593b5
Stefano Montesi <dincio.montesi@gmail.com>
2018-08-02 13:18:38 +0200

    Modified make_unique_vector interface to make it work on polymorphic factory functions.

--- memory.h
+++ memory.h
@@ -6,12 +6,14 @@
 namespace utl {
 
 // construct vector of unique_ptrs
-template <class T, class... Args>
+template <class Base, class... Args>
 auto make_unique_vector(Args&&... args) {
-    std::vector<std::unique_ptr<T>> v;
-    v.reserve(sizeof...(args));
-    (v.emplace_back(std::make_unique<T>(std::forward<Args>(args))), ...);
-    return v;
+    std::vector<std::unique_ptr<Base>> retVal;
+
+    retVal.reserve(sizeof...(args));
+    (retVal.emplace_back(std::forward<Args>(args)), ...);
+
+    return retVal;
 }
 
 }
